f6421270d507f9b28570b6c20385d172b0e57591
rongrhu <31623990+rongrhu@users.noreply.github.com>
2018-04-09 09:33:41 +0200

    Add files via upload

---
+++ matrix.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+
+
+    // === Sidebar navigation === //
+
+    $('.submenu > a').click(function(e) {
+        e.preventDefault();
+        var submenu = $(this).siblings('ul');
+        var li = $(this).parents('li');
+        var submenus = $('#sidebar li.submenu ul');
+        var submenus_parents = $('#sidebar li.submenu');
+        if (li.hasClass('open')) {
+            if (($(window).width() > 768) || ($(window).width() < 479)) {
+                submenu.slideUp();
+            } else {
+                submenu.fadeOut(250);
+            }
+            li.removeClass('open');
+        } else {
+            if (($(window).width() > 768) || ($(window).width() < 479)) {
+                submenus.slideUp();
+                submenu.slideDown();
+            } else {
+                submenus.fadeOut(250);
+                submenu.fadeIn(250);
+            }
+            submenus_parents.removeClass('open');
+            li.addClass('open');
+        }
+    });
+
+
+
+
+
+
+    // === Style switcher === //
+
+
+
+
+    $('.lightbox_trigger').click(function(e) {
+
+        e.preventDefault();
+
+        var image_href = $(this).attr("href");
+
+
+
+    });
+
+
+
+
+});
